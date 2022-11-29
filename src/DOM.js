994c5f3dbd6a97232d69d044e073d75583da3bf7
Dayan <m.dayan5689@gmail.com>
2022-11-29 09:42:26 -0800

    Add function to render weather info to DOM

---
+++ src/DOM.js
@@ -0,0 +1,20 @@
+const DOM = (arr) => {
+  const body = document.querySelector("#content");
+  body.innerHTML = "";
+  const cont = document.createElement("div");
+  const name = document.createElement("h1");
+  name.textContent = arr[0];
+  const temperature = document.createElement("h2");
+  temperature.textContent = Math.round(arr[1].temp - 273, 2) + "\u00B0 C";
+  const feels_like = document.createElement("span");
+  feels_like.textContent = `Feels like: ${Math.round(
+    arr[1].feels_like - 273,
+    2
+  )}\u00B0 C`;
+  const description = document.createElement("h2");
+  description.textContent = arr[2][0].main;
+  cont.append(name, description, temperature, feels_like);
+  body.append(cont);
+};
+
+export { DOM };
--- src/fetch.js
+++ src/fetch.js
@@ -1,15 +1,20 @@
+import { DOM } from "./DOM";
+
+
 async function getData() {
-  try{
-  const location = document.querySelector("#city").value;
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2f4907111f7679621a12fd4616f08931`,
-    { mode: "cors" }
-  );
-  const data = await response.json();
-  console.log(data);
-  }
-  catch (err){
-    console.log(err)
+  try {
+    const location = document.querySelector("#city").value;
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2f4907111f7679621a12fd4616f08931`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    const weather = [];
+    weather.push(data.name, data.main, data.weather);
+    DOM(weather);
+    console.log(weather);
+  } catch (err) {
+    console.log(err);
   }
 }
 
